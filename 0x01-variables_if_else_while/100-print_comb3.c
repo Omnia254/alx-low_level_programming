695117dd512763b5d6c24faefbe8bd829c45a6f4
Omnia254 <omniasayed689@gmail.com>
2023-03-17 03:23:59 -0700

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of two different digits,
- *        in ascending order, separated by a comma followed by a space.
+ * main - Prints all possibile combination.
  *
  * Return: Always 0.
  */
